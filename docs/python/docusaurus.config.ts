4a69e926ca6b561c9995150820b7bebaa4fc6de6
Anthony Li <38049831+doccstat@users.noreply.github.com>
2025-06-16 04:20:49 -0500

    Fix broken links.

--- docs/python/docusaurus.config.ts
+++ docs/python/docusaurus.config.ts
@@ -105,7 +105,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/main-function',
             },
           ],
         },
